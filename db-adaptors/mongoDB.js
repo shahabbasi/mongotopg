78362b8d7fe1f542cc6e768c87592baf171b4336
Ali Shahabbasi <shahabbasi.ali.98@gmail.com>
2021-09-28 13:32:01 +0330

    Add mongodb adaptor

---
+++ db-adaptors/mongoDB.js
@@ -0,0 +1,37 @@
+const { MongoClient } = require('mongodb');
+
+
+const {
+    MONGO_URL,
+    MONGO_HOST = '127.0.0.1',
+    MONGO_PORT = '27017',
+    MONGO_USER = '',
+    MONGO_PASSWORD = '',
+    MONGO_DB = '',
+} = process.env;
+let mongoClient = null;
+
+
+function getMongoUrl () {
+    if (MONGO_URL) return MONGO_URL;
+    return `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DB}`;
+}
+
+async function getMongoClient (client = null) {
+    if (client) return client;
+    if (mongoClient) return mongoClient;
+    client = new MongoClient(getMongoUrl());
+
+    try {
+        await client.connect();
+        await  listDatabases(client);
+        mongoClient = client;
+    } catch (e) {
+        console.error(e);
+        throw e;
+    } finally {
+        return client;
+    }
+}
+
+module.exports = { getMongoClient };
--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 const { RedisClient, getRedisClient } = require('./db-adaptors/redis');
 const { getPostgresClient } = require('./db-adaptors/postgreSQL');
+const { getMongoClient } = require('./db-adaptors/mongoDB');
 
-const redis = getRedisClient();
-const redisClient = new RedisClient(redis);
-const pgClient = getPostgresClient();
-
+async function init () {
+    const redis = getRedisClient();
+    const redisAsyncClient = new RedisClient(redis);
+    const pgClient = getPostgresClient();
+    const mongoClient = await getMongoClient();
+}
